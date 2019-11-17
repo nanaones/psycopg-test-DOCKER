3037befd27b51f91cbf4e1728b38d3468f27a83d
nanaones <nara03050@gmail.com>
2019-11-18 00:43:38 +0900

    add config file

--- dockerfile/start/start.sh
+++ dockerfile/start/start.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
 
-git clone https://github.com/nanaones/psycopg-test-docker &&\
-git cline https://github.com/vishnubob/wait-for-it &&\
+git clone https://github.com/nanaones/psycopg-test &&\
+# git clone https://github.com/vishnubob/wait-for-it &&\
 
 ## get env if run as docker-compose
 ## TODO add if connectPgcontainer, connect elasticContainer and  fluentdContainer 
 
 # wait-for-it
-
-cd  /psycopg-test-docker/ &&\
+mkdir /logs &&\
+ls &&\
+cat /run/config/config.ini &&\
+cd  /psycopg-test/ &&\
+ls &&\
+cp /run/config/config.ini /psycopg-test/config/config.ini &&\
 python3 -m pip install -r requirements.txt &&\
 python3 main.py  $loop
